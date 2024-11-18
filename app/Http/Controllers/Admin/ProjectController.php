73526f9b69df7a3a3a9871fcaee1854858c6927a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-11-18 16:52:52 +0100

    modifica create e edit nel controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -23,7 +24,7 @@ class ProjectController extends Controller
     public function create()
     {
 
-        //$types = Type::all();
+        $types = Type::all();
 
         //$technologies = Technology::all();
 
@@ -60,6 +61,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
+        $types = Type::all();
         return view('admin.projects.edit', compact('project'));
     }
 
